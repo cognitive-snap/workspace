07cd5811ade7ce5ec04a374f0aa4eebfd281a34b
mengxinwb <1518911908@qq.com>
2019-03-28 16:17:24 +0800

    增加青岛中院大屏开发

---
+++ 青岛中院V2.0/数据对接/sql/首页/sy.sql
@@ -0,0 +1,240 @@
+drop view if exists db_dpzjk.v_visu_sy;
+create view db_dpzjk.v_visu_sy as
+
+--质效指标排行
+select 
+  public.uuid_generate_v1() as id,
+  'sy_zxzbph'as schemaid,
+  now() as gathertime,
+  now() as recordtime,
+  null as columnvalue1,
+  null as columnvalue2,
+  null as columnvalue3,
+  null as columnvalue4,
+  null as columnvalue5,
+  null as columnvalue6,
+  null as columnvalue7 ,
+  null as columnvalue8,
+  null as columnvalue9,
+  null as columnvalue10,
+  null as columnvalue11,
+  null as columnvalue12,
+  null as columnvalue13,
+  null as columnvalue14 ,
+  null as columnvalue15 ,
+  null as columnvalue16 ,
+  null as columnvalue17 ,
+  null as columnvalue18 ,
+  null as columnvalue19 ,
+  null as columnvalue20 ,
+  null as columnvalue21 ,
+  null as columnvalue22 ,
+  null as columnvalue23 ,
+  null as columnvalue24 ,
+  null as columnvalue25 ,
+  null as columnvalue26 ,
+  null as columnvalue27 ,
+  null as columnvalue28 ,
+  null as columnvalue29 ,
+  null as columnvalue30 ,
+  null as extendvalue 
+from (
+
+
+
+)zxzbph
+union all 
+--首页案件数
+select 
+  public.uuid_generate_v1() as id,
+  'sy_syajs' as schemaid,
+  now() as gathertime,
+  now() as recordtime,
+  null as columnvalue1,
+  null as columnvalue2,
+  null as columnvalue3,
+  null as columnvalue4,
+  null as columnvalue5,
+  null as columnvalue6,
+  null as columnvalue7 ,
+  null as columnvalue8,
+  null as columnvalue9,
+  null as columnvalue10,
+  null as columnvalue11,
+  null as columnvalue12,
+  null as columnvalue13,
+  null as columnvalue14 ,
+  null as columnvalue15 ,
+  null as columnvalue16 ,
+  null as columnvalue17 ,
+  null as columnvalue18 ,
+  null as columnvalue19 ,
+  null as columnvalue20 ,
+  null as columnvalue21 ,
+  null as columnvalue22 ,
+  null as columnvalue23 ,
+  null as columnvalue24 ,
+  null as columnvalue25 ,
+  null as columnvalue26 ,
+  null as columnvalue27 ,
+  null as columnvalue28 ,
+  null as columnvalue29 ,
+  null as columnvalue30 ,
+  null as extendvalue 
+from (
+
+
+
+)syajs
+union all 
+-- 涉及特殊主体
+select 
+  public.uuid_generate_v1() as id,
+  'sy_stszt' as schemaid,
+  now() as gathertime,
+  now() as recordtime,
+  null as columnvalue1,
+  null as columnvalue2,
+  null as columnvalue3,
+  null as columnvalue4,
+  null as columnvalue5,
+  null as columnvalue6,
+  null as columnvalue7,
+  null as columnvalue8,
+  null as columnvalue9,
+  null as columnvalue10,
+  null as columnvalue11,
+  null as columnvalue12,
+  null as columnvalue13,
+  null as columnvalue14 ,
+  null as columnvalue15 ,
+  null as columnvalue16 ,
+  null as columnvalue17 ,
+  null as columnvalue18 ,
+  null as columnvalue19 ,
+  null as columnvalue20 ,
+  null as columnvalue21 ,
+  null as columnvalue22 ,
+  null as columnvalue23 ,
+  null as columnvalue24 ,
+  null as columnvalue25 ,
+  null as columnvalue26 ,
+  null as columnvalue27 ,
+  null as columnvalue28 ,
+  null as columnvalue29 ,
+  null as columnvalue30 ,
+  null as extendvalue 
+from (
+
+
+
+)sjtszt
+union all 
+--信用惩戒
+
+select 
+  public.uuid_generate_v1() as id,
+  'sy_xycj' as schemaid,
+  now() as gathertime,
+  now() as recordtime,
+  null as columnvalue1,
+  null as columnvalue2,
+  null as columnvalue3,
+  null as columnvalue4,
+  null as columnvalue5,
+  null as columnvalue6,
+  null as columnvalue7,
+  null as columnvalue8,
+  null as columnvalue9,
+  null as columnvalue10,
+  null as columnvalue11,
+  null as columnvalue12,
+  null as columnvalue13,
+  null as columnvalue14 ,
+  null as columnvalue15 ,
+  null as columnvalue16 ,
+  null as columnvalue17 ,
+  null as columnvalue18 ,
+  null as columnvalue19 ,
+  null as columnvalue20 ,
+  null as columnvalue21 ,
+  null as columnvalue22 ,
+  null as columnvalue23 ,
+  null as columnvalue24 ,
+  null as columnvalue25 ,
+  null as columnvalue26 ,
+  null as columnvalue27 ,
+  null as columnvalue28 ,
+  null as columnvalue29 ,
+  null as columnvalue30 ,
+  null as extendvalue 
+from (
+
+
+
+)xycj
+union all
+
+--限制人次
+select 
+  public.uuid_generate_v1() as id,
+  'sy_xzrc' as schemaid,
+  now() as gathertime,
+  now() as recordtime,
+  null as columnvalue1,
+  null as columnvalue2,
+  null as columnvalue3,
+  null as columnvalue4,
+  null as columnvalue5,
+  null as columnvalue6,
+  null as columnvalue7,
+  null as columnvalue8,
+  null as columnvalue9,
+  null as columnvalue10,
+  null as columnvalue11,
+  null as columnvalue12,
+  null as columnvalue13,
+  null as columnvalue14 ,
+  null as columnvalue15 ,
+  null as columnvalue16 ,
+  null as columnvalue17 ,
+  null as columnvalue18 ,
+  null as columnvalue19 ,
+  null as columnvalue20 ,
+  null as columnvalue21 ,
+  null as columnvalue22 ,
+  null as columnvalue23 ,
+  null as columnvalue24 ,
+  null as columnvalue25 ,
+  null as columnvalue26 ,
+  null as columnvalue27 ,
+  null as columnvalue28 ,
+  null as columnvalue29 ,
+  null as columnvalue30 ,
+  null as extendvalue 
+from (
+
+
+
+)xzrc
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
